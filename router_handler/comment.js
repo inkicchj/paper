08af33dd6153500c4e0f5a434ea887a355d6aa1f
unknown <399434428@qq.com>
2022-06-04 15:07:59 +0800

    paper_1.0.1

--- router_handler/comment.js
+++ router_handler/comment.js
@@ -5,13 +5,12 @@ const db = require("../db_handler/index")
 exports.getCommentList = (req, res) => {
     const pageSize = req.body.pageSize
     const pageNum = req.body.pageNum - 1
-
+    // 查询动态是否存在
     const sql_post = 'select * from ev_posts where post_id = ? and post_is_delete = 0'
-
     db.query(sql_post, req.body.post_id, (err, result) => {
         if (err) return res.cc(err)
-        if (result.length === 0) return res.cc("文章不存在")
-        
+        if (result.length === 0) return res.cc("动态不存在")
+        // 查询留言列表
         const sql = `SELECT co.comment_id, co.comment_text, co.comment_pub_date, u.user_id, u.user_nickname, u.user_avatar 
         FROM ev_comments co 
         INNER JOIN ev_posts p 
@@ -24,7 +23,6 @@ exports.getCommentList = (req, res) => {
         AND p.post_is_delete = 0
         ORDER BY co.comment_id ASC
         LIMIT ${pageSize*pageNum}, ${pageSize}`
-
         db.query(sql, req.body.post_id, (err, result1) => {
             if (err) return res.cc(err)
             if (result1.length === 0) return res.cc("未获取到留言数据")
@@ -35,14 +33,13 @@ exports.getCommentList = (req, res) => {
             })
         })
     })
-
-
 }
 
 // 根据用户 id 获取留言列表
 exports.getCommentListByUser = (req, res) => {
     const pageSize = req.body.pageSize
     const pageNum = req.body.pageNum - 1
+    // 查询留言列表
     const sql = `SELECT co.comment_id, co.comment_text, co.comment_pub_date, u.user_id, u.user_nickname, u.user_avatar, p.post_id, p.post_title, p.post_text 
                 FROM ev_comments co 
                 INNER JOIN ev_posts p 
@@ -65,25 +62,21 @@ exports.getCommentListByUser = (req, res) => {
     })
 }
 
-// 新增评论数据
+// 发布留言
 exports.addComment = (req, res) => {
-
     const commentInfo = {
         ...req.body,
         comment_pub_date: new Date(),
         user_id: req.auth.user_id,
     }
-
     const sql_post = 'select * from ev_posts where post_id = ? and post_is_delete = 0'
-
     db.query(sql_post, req.body.post_id, (err, result) => {
         if (err) return res.cc(err)
-        if (result.length === 0) return res.cc("文章不存在")
-        const sql = 'insert into ev_comments set ?' // 新增文章
+        if (result.length === 0) return res.cc("留言不存在")
+        const sql = 'insert into ev_comments set ?' // 新增留言
         db.query(sql, commentInfo, (err, result1) => {
             if (err) return res.cc(err)
             if (result1.affectedRows !== 1) return res.cc("发布留言失败")
-
             commentInfo.user_nickname = req.auth.user_nickname
             commentInfo.user_avatar = req.auth.user_avatar
             res.send({
@@ -95,21 +88,16 @@ exports.addComment = (req, res) => {
     }) 
 }
 
-// 根据 id 删除评论
+// 根据 id 删除留言
 exports.deleteCommentById = (req, res) => {
-
     const sql = 'select * from ev_comments where comment_id = ?'
     db.query(sql, req.body.comment_id, (err, result) => {
-
         if (err) return res.cc(err)
         if (result.length === 0) return res.cc("未查找到该留言")
-
         const update_sql = 'update ev_comments set comment_is_delete = 1 where comment_id = ?'
         db.query(update_sql, req.body.comment_id, (err, result) => {
-
             if (err) return res.cc(err)
             if (result.affectedRows !== 1) return res.cc("删除留言失败")
-
             res.cc("删除留言成功", 1)
         })
     })
